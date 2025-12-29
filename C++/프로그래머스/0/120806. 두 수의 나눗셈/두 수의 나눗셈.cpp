a31170606ac40e7cbcaf97b80683bd43ea1782f3
TaeWoo Kim <98134264+xodn246@users.noreply.github.com>
2025-12-29 09:19:40 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    float div = 0;
+    div = ((float)num1 / (float)num2) * 1000;
+    answer = div;
+    
+    
+    return answer;
+}
