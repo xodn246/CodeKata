ce6bd6e387175f76d1f800f16e1c1149d3dd52e0
TaeWoo Kim <98134264+xodn246@users.noreply.github.com>
2025-12-30 09:04:53 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 4.03 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120829. 각도기/각도기.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int angle) {
+    int answer = 0;
+    if(angle < 90)
+    {
+        answer = 1;
+    }
+    else if (angle == 90)
+    {
+        answer = 2;
+    }
+    else if (angle > 90 && angle < 180)
+    {
+        answer = 3;
+    }
+    else if (angle == 180)
+    {
+        answer = 4;
+    }
+    else
+    {
+        answer = -1;
+    }
+    return answer;
+}
