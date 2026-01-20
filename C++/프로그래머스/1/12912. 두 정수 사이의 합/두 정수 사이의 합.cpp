97662a6bfbdbc3253afa0b590e51b905ff7751f0
TaeWoo Kim <98134264+xodn246@users.noreply.github.com>
2026-01-20 09:51:56 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ C++/프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+long long solution(int a, int b) {
+    long long answer = 0;
+    long long numCount;
+    
+    numCount = llabs(b - a) + 1;
+    
+    answer = (a + b) * numCount / 2;
+    return answer;
+}
