6b9f3ae90508d3c1d530f545ae46ca73cc0af1ea
TaeWoo Kim <98134264+xodn246@users.noreply.github.com>
2026-01-28 09:35:14 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ C++/프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+    int answer = 0;
+    vector<bool> numCheck(10, false);
+    
+    for(int i : numbers)
+    {
+        numCheck[i] = true;
+    }
+    
+    for(int i = 0 ; i < numCheck.size(); ++i)
+    {
+        if(!numCheck[i]) answer += i;
+    }
+    
+    return answer;
+}
