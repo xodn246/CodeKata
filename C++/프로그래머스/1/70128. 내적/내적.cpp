f48e3f082932ca4c217251ff0500a247e39d5b61
TaeWoo Kim <98134264+xodn246@users.noreply.github.com>
2026-02-03 09:33:46 +0900

    [level 1] Title: 내적, Time: 0.01 ms, Memory: 3.69 MB -BaekjoonHub

---
+++ C++/프로그래머스/1/70128. 내적/내적.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> a, vector<int> b) {
+    int answer = 0;
+    int SIZE = a.size();
+    
+    for (int i = 0 ; i < SIZE ; ++i)
+    {
+        answer += a[i] * b[i];
+    }
+    
+    return answer;
+}
